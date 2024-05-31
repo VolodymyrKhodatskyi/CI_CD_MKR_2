1b47e59ae18423715b330c9db149c8e50f2663cf
VolodymyrKhodatskyi <datomir777@gmail.com>
2024-05-31 15:04:40 +0300

    updated views

--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from recipe.models import Recipe, Category
 
 def main(request):
-    return render(request, 'main.html')
+    Recipe.objects.create(title="Test", description="Test", instructions="Test", ingredients="Test")
+    recipes =   Recipe.objects.all()
+    context = {'recipes': recipes}
+    return render(request, 'main.html', context)
+
+
 
 def category_list(request):
-    return render(request, 'category_list.html')
+    Category.objects.create(name="Test1")
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'category_list.html', context)
